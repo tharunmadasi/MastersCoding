207061087957e2c05b86ae7556ad87e262b081c7
Thoparam-Sai-nithish <thoparamsainithish1234@gmail.com>
2023-09-18 01:10:41 +0530

    Mentor front end started!

--- src/Components/Mentor/MentorSidebar/MtrAssignments/MtrAssignments.js
+++ src/Components/Mentor/MentorSidebar/MtrAssignments/MtrAssignments.js
@@ -3,50 +3,93 @@ import "./MtrAssignments.css";
 import axios from "axios";
 import Select from 'react-select' 
 import { AssignmentContext } from "../../../../Contexts/AssignmentContext";
+import { useForm } from "react-hook-form";
 
 function MtrAssignments(props) {
+
+  //form to select the assignment
+  let {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    if (data.assignmentID == "select assignmentID") setAssignmentError(true);
+    else {
+      setAssignmentError(false);
+      console.log("Assignment no.:",data);
+    }
+  };
+
+  //states
   const [links, setLinks] = useState([]);
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
   const [assignmentCounter] = useContext(AssignmentContext);
 
+  //state to check the errors in the batch  select input
+  const [assignmentError, setAssignmentError] = useState(false);
+  const [assignmentOptions , setAssignmentOptions] = useState([]);
+
   useEffect(() => {
-    axios
-      .get("http://localhost:3500/assignments/AllAssignments")
-      .then((res) => {
-        const sortedAssignments = res.data.assignments.sort((a, b) => {
-          const timestampA = new Date(a.timestamp).getTime();
-          const timestampB = new Date(b.timestamp).getTime();
-          return timestampB - timestampA;
-        });
-        setLinks(sortedAssignments);
-      })
-      .catch((err) => console.log(err));
-    axios
-      .get("http://localhost:3500/submissions/AllSubmissions")
-      .then((res) =>
-        console.log(res.data)
-      )
-      .catch((err) => console.log(err));
+    // axios
+    //   .get("http://localhost:3500/assignments/AllAssignments")
+    //   .then((res) => {
+    //     const sortedAssignments = res.data.assignments.sort((a, b) => {
+    //       const timestampA = new Date(a.timestamp).getTime();
+    //       const timestampB = new Date(b.timestamp).getTime();
+    //       return timestampB - timestampA;
+    //     });
+    //     setLinks(sortedAssignments);
+    //   })
+    //   .catch((err) => console.log(err));
+    // axios
+    //   .get("http://localhost:3500/submissions/AllSubmissions")
+    //   .then((res) =>
+    //     console.log(res.data)
+    //   )
+    //   .catch((err) => console.log(err));
 
   }, []);
+
+  useEffect(()=>{
+    setAssignmentOptions(['1','2','3','4','5','6','7','8','9' ])
+  },[])
 
   const handleCardClick = (url) => {
     window.open(url, "_blank");
   };
-
+  
   return (
     <div className="container">
-        <Select options={options} />
+     
+      <form  className="mb-4 " onSubmit={handleSubmit(onSubmit)}>
+        {/* select batch */}
+        <div className="row gap-4">
+          <div className="col-md-2 border-success border rounded bg-success    pt-1">Select Assignment</div>
+          <select
+              className="mt-1 col-md-8 "
+              name="assignmentID"
+              id="assignmentID"
+              {...register("assignmentID", {
+                required: "assignmentID is required",
+              })}
+            >
+              <option disabled selected value="select assignmentID">
+                Select assignmentID
+              </option>
+              {assignmentOptions?.map((value,ind) => (
+                <option key={ind} value={value}>{value} </option>
+              ))}
+            </select>
+            
+
+            <button type="submit" className="btn btn-success col-md-1" >Get</button>
+          </div>
+          {assignmentError == true && (
+              <p className="text-danger">{"assignmentID is required"}</p>
+            )}
+        </form>
 
       <div className="row">
         <div className="col-md-6">
